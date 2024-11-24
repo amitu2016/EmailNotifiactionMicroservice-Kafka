736b933c657d2f8229682825a5d6637cfaf32949
amit-macbook <amiu2016@gmail.com>
2024-11-24 21:31:04 +0530

    commit

---
+++ src/main/java/com/appsdeveloperblog/ws/emailnotification/error/NonRetrableException.java
@@ -0,0 +1,20 @@
+package com.appsdeveloperblog.ws.emailnotification.error;
+
+public class NonRetrableException extends RuntimeException {
+
+
+	private static final long serialVersionUID = 1L;
+
+	public NonRetrableException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NonRetrableException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
---
+++ src/main/java/com/appsdeveloperblog/ws/emailnotification/error/RetryableException.java
@@ -0,0 +1,17 @@
+package com.appsdeveloperblog.ws.emailnotification.error;
+
+public class RetryableException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public RetryableException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RetryableException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
